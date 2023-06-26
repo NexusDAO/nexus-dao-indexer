f8199942a61b73aeea250929228905e0f3b0ada5
tian zhou <anynya@qq.com>
2023-06-26 10:24:32 +0800

    Update routes.rs

--- src/routes.rs
+++ src/routes.rs
@@ -2,7 +2,8 @@ use crate::handlers::{records_handler, get_profile_handler};
 use axum::{routing::get, Router};
 
 pub fn routes() -> Router {
-    Router::new().route("/records", get(records_handler));
-    Router::new().route("/profiles", get(get_profile_handler))
+    Router::new()
+        .route("/records", get(records_handler))
+        .route("/profiles", get(get_profile_handler))
 }
 
